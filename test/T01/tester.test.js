a34b7080794c1e40f7687fead76e8a67e596e5a5
dpm3354 <paul.merrill@beaufortfairmont.com>
2020-04-03 13:38:56 -0400

    Adding a coding exercise to T01.

---
+++ test/T01/tester.test.js
@@ -0,0 +1,34 @@
+//const Tester = require("./tester").tester;
+var assert = require('assert');
+var tester = require('./tester.js');
+
+
+function isJsonString (jsonString){
+    try {
+        var o = JSON.parse(jsonString);
+
+        if (o && typeof o === "object") {
+            return o;
+        }
+    }
+    catch (e) { }
+
+    return false;
+};
+
+// Two things that
+describe("Tester Test", () => {
+    describe("JSON", () => {
+        test('Test JSON is syntactically correct', () => {
+            assert.ok(isJsonString(JSON.stringify(tester)), "syntax is not correct");
+        });
+    });
+    describe("Tester contains correct properties", () => {
+        test('Test name exists', () => {
+            assert.ok(tester.Name, "this tester should have a name");
+        });
+        test('Test warm body', () => {
+            assert.equal(tester.warmBody, true, "this tester should have a warm body");
+        });
+    });
+});
